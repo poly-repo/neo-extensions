9d7de19aee4c0e2824c372a876134a10e96a95d5
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-10 20:40:26 -0400

    Add a first extension
    Not much, just to prime the pump and get things rolling
    
    GitOrigin-RevId: cbb31547310fa60a9eaf8ded303bd72b6195c28b

---
+++ extensions/uno/questionable_defaults/manifest.el
@@ -0,0 +1,9 @@
+(neo/extension
+ :name "Questionable Defaults"
+ :author "Maurizio Vitale"
+ :forge "github"
+ :user "poly-repo"
+ :repo "neo-extensions"
+ :path "extensions/uno/questionable_defaults"
+ :description "A set of defaults that the author find cool and others might find less cool"
+ )
