0a40bc4dab86ad992644d9c2430204b4d5a560fc
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-10 21:24:06 -0400

    Test of building a full registry summary
    This includes the emblem and the complete readme file.
    Just an experiment, most likely well split downloads.
    
    GitOrigin-RevId: 2c6b8d25fca7d2211467d4990218a0f2a8ef5beb

--- build/extensions.el
+++ build/extensions.el
@@ -1,16 +1,50 @@
-;;; build/extensions.el --- Batch build for questionable_defaults
+;; ;;; build/extensions.el --- Batch build for questionable_defaults
+
+;; (setq input-file "extensions/uno/questionable_defaults/manifest.el")
+;; (setq output-file "dist/neo-extensions.el")
+;; (setq n 10000)
+
+;; (with-temp-buffer
+;;   (dotimes (_ n)
+;;     (insert-file-contents input-file)
+;;     (goto-char (point-max)))
+;;   (write-region (point-min) (point-max) output-file))
+
+;; ;;; extensions.el ends here
+
+
+
+;;; build/extensions.el --- Batch build for neo extensions with raw binary emblem
 
 (setq input-file "extensions/uno/questionable_defaults/manifest.el")
+(setq emblem-file "extensions/uno/questionable_defaults/emblem.png")
 (setq output-file "dist/neo-extensions.el")
-(setq n 10000)
+(setq n 3)
+
+(defun read-binary-file (file)
+  "Return the contents of FILE as a raw string (not base64)."
+  (with-temp-buffer
+    (insert-file-contents-literally file)
+    (buffer-string)))
+
+(setq emblem-bytes (read-binary-file emblem-file))
+
+(defun embed-emblem-in-extension (form)
+  "Return FORM with :emblem key containing raw PNG bytes."
+  (let ((new-form (append form (list :emblem emblem-bytes))))
+    new-form))
 
 (with-temp-buffer
   (dotimes (_ n)
-    (insert-file-contents input-file)
-    (goto-char (point-max)))
+    (let ((data (with-temp-buffer
+                  (insert-file-contents input-file)
+                  (read (current-buffer)))))
+      ;; Serialize with raw string printing
+      (let ((print-escape-nonascii nil) ; allow raw binary
+            (print-length nil)
+            (print-level nil))
+        (prin1 (embed-emblem-in-extension data) (current-buffer))
+        (terpri (current-buffer)))))
   (write-region (point-min) (point-max) output-file))
 
 ;;; extensions.el ends here
-
-
-
