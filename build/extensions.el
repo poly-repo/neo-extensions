bb28262ab07ce80451f2102b778e3f2fcdca6b48
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-10 20:56:49 -0400

    First implementation of publishing extensions
    We have a github action that produces a number of assets.
    
    Initially this will only be a number of copies of the one extension we have.
    
    GitOrigin-RevId: ea8a6e42a71107f1f4ff5657a13ef98f0a008f3b

---
+++ build/extensions.el
@@ -0,0 +1,16 @@
+;;; build/extensions.el --- Batch build for questionable_defaults
+
+(setq input-file "extensions/uno/questionable_defaults/manifest.el")
+(setq output-file "dist/neo-extensions.el")
+(setq n 3)
+
+(with-temp-buffer
+  (dotimes (_ n)
+    (insert-file-contents input-file)
+    (goto-char (point-max)))
+  (write-region (point-min) (point-max) output-file))
+
+;;; extensions.el ends here
+
+
+
