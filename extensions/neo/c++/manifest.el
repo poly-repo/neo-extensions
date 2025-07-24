5332364333783264ce04a155b80c90ee5b102af6
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-23 21:07:06 -0400

    Add a mostly empty extension for C++ programming
    Needed to prime the pump for development
    
    GitOrigin-RevId: 89577dec3eb81dfb5bc19d187b7a9f6283e6df6b

---
+++ extensions/neo/c++/manifest.el
@@ -0,0 +1,12 @@
+(neo/extension
+ :name "C++"
+ :title "Making C++ development almost pleasant. Almost."
+ :publisher "neo"
+ :description "Settings for helping with C++ development. You bring your own bugs."
+ :categories (neo)
+ :keywords (programming c++)
+ :requires ()
+ :repository (
+	      :type "git"
+	      :url "https://github.com/poly-repo/neo-extensions.git"
+	      :path "extensions/uno/c++"))
