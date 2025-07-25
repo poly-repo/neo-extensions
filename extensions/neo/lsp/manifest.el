8033da26462901a12d5edbf7f13aecd303071840
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-25 15:17:24 -0400

    Add the beginning of an extension for LSP
    GitOrigin-RevId: 3c0cd3a8a3527f93d31c23157a2a8996961ea42e

---
+++ extensions/neo/lsp/manifest.el
@@ -0,0 +1,12 @@
+(neo/extension
+ :name "lsp"
+ :title "Language Server Protocol"
+ :publisher "neo"
+ :description "What used to be cool for programming before vibe coding"
+ :categories (neo)
+ :keywords (programming lsp)
+ :requires ()
+ :repository (
+	      :type "git"
+	      :url "https://github.com/poly-repo/neo-extensions.git"
+	      :path "extensions/uno/lsp"))
