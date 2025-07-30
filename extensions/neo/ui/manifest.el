fafab7ff235da6f88b3b92a1ad0ffd44cec4c494
Maurizio Vitale <mrz.vtl+github@gmail.com>
2025-07-30 13:28:13 -0400

    Beginning of the UI config extension
    GitOrigin-RevId: 1f1cf56272c5371c243ddc2623f2b00e9f796e5a

---
+++ extensions/neo/ui/manifest.el
@@ -0,0 +1,12 @@
+(neo/extension
+ :name "UI"
+ :title "UI settings, fonts, themes"
+ :publisher "neo"
+ :description "Turning Emacs from ‘what is this?’ to ‘whoa, nice!"
+ :categories (neo)
+ :keywords (ui appearance)
+ :requires ()
+ :repository (
+	      :type "git"
+	      :url "https://github.com/poly-repo/neo-extensions.git"
+	      :path "extensions/uno/ui"))
