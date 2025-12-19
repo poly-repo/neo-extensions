4c53c0d4a5006aa86dc476556a075bbbd0bd9104
Maurizio Vitale <109253047+m-vitale@users.noreply.github.com>
2025-12-18 23:36:10 -0500

    ✨ feat(neo): Add a python extension (#89)
    Enough to get treesitter. More work is needed for bazel integration
    and eglot support.
    
    GitOrigin-RevId: 142884cfe1e32a61ac916023fac9e3966da3471f

---
+++ extensions/neo/python/manifest.el
@@ -0,0 +1,13 @@
+(neo/extension
+ :name "Python"
+ :title "Tame the snake—Emacs style."
+ :publisher "neo"
+ :description "Python support."
+ :categories (neo)
+ :keywords (programming python)
+ :requires ()
+ :depends-on ("neo:programming-foundation")
+ :repository (
+	      :type "git"
+	      :url "https://github.com/poly-repo/neo-extensions.git"
+	      :path "extensions/neo/python"))
