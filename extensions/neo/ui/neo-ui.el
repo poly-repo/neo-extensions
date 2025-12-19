c59f4f415945e1646f0dcba8e8def382607d69df
Maurizio Vitale <109253047+m-vitale@users.noreply.github.com>
2025-12-19 10:25:34 -0500

    ✨ feat(neo): Add spacious padding to NEO (#91)
    I mostly like it but it plays badly with some modes (magit's commit
    window for instance). This seems limited to the modeline.
    
    GitOrigin-RevId: 2858f95c161b720864e2b9d53a91eec30466a2ce

--- extensions/neo/ui/neo-ui.el
+++ extensions/neo/ui/neo-ui.el
@@ -10,3 +10,30 @@
   :config
   (winum-mode 1))
 
+(neo/use-package spacious-padding
+  :ensure t
+  :config
+  ;; These are the default values, but I keep them here for visibility.
+  ;; Also check `spacious-padding-subtle-frame-lines'.
+  (setq spacious-padding-widths
+        '( :internal-border-width 15
+           :header-line-width 4
+           :mode-line-width 6
+           :custom-button-width 3
+           :tab-width 4
+           :right-divider-width 30
+           :scroll-bar-width 8
+           :fringe-width 8))
+  (spacious-padding-mode 1))
+
+(neo/use-package emacs
+  :config
+  (global-hl-line-mode 1))
+
+;; TODO: not sure what these do
+;; (setq spacious-padding-subtle-frame-lines
+;;       '( :mode-line-active spacious-padding-line-active
+;;          :mode-line-inactive spacious-padding-line-inactive
+;;          :header-line-active spacious-padding-line-active
+;;          :header-line-inactive spacious-padding-line-inactive))
+
