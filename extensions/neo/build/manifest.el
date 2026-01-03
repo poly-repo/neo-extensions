6c24dda3e82248c8f70d7aa7d661a8f7686caf65
Maurizio Vitale <109253047+m-vitale@users.noreply.github.com>
2026-01-03 17:31:37 -0500

    ✨ feat(neo): Add build extension supporting bazel and the starpls LSP server (#223)
    Closes: #222
    GitOrigin-RevId: 3df790d8301f5dbeb3b9203c672c28f17d3005e1

---
+++ extensions/neo/build/manifest.el
@@ -0,0 +1,13 @@
+(neo/extension
+ :name "build"
+ :title "Build tools support"
+ :publisher "neo"
+ :description "A unifying theory of builds and buffers\n\n"
+ :categories (neo) ; not sure what the intention for this was
+ :keywords (tools programming)
+
+ :requires ()
+ :repository (
+	      :type "git"
+	      :url "https://github.com/poly-repo/neo-extensions.git"
+	      :path "extensions/neo/build"))
