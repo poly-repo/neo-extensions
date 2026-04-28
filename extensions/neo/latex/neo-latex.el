447638d7a6bc98c657adc81cd2ea39176f69183c
Maurizio Vitale <mrz.vtl+github@gmail.com>
2026-04-28 15:36:24 -0400

    Add LaTeX code block editor
    GitOrigin-RevId: d9b358d3cef6bd77f7b84ea60103863ef1c495f5

--- extensions/neo/latex/neo-latex.el
+++ extensions/neo/latex/neo-latex.el
@@ -69,6 +69,7 @@
 
                              (define-key LaTeX-mode-map (kbd "C-c C-p") 'reftex-parse-all)
                              (define-key LaTeX-mode-map (kbd "C-c C-g") #'pdf-sync-forward-search)
+			     (define-key LaTeX-mode-map (kbd "C-c '") #'neo/edit-code-block)
 
                              (setq LaTeX-section-hook
                                    '(LaTeX-section-heading
@@ -89,5 +90,141 @@
                                        #'TeX-revert-document-buffer) ;
                              (add-hook 'pdf-view-mode-hook (lambda() (display-line-numbers-mode -1)))
                              ))
+
+(defun neo/starlark--bounds-auctex-precise ()
+  "Return the bounds of the current starlark environment body."
+  (save-excursion
+    (unless (string= (LaTeX-current-environment) "starlark")
+      (user-error "Not inside a starlark environment"))
+
+    ;; Start after the newline that ends the \begin line.
+    (LaTeX-find-matching-begin)
+    (end-of-line)
+    (let ((beg (if (eobp) (point) (1+ (point)))))
+
+      ;; Stop at the beginning of the \end line.
+      (LaTeX-find-matching-end)
+      (beginning-of-line)
+      (let ((end (point)))
+        (cons beg end)))))
+
+(defvar neo--latex-code-edit-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c C-c") #'neo--latex-code-edit-accept)
+    (define-key map (kbd "C-c C-k") #'neo--latex-code-edit-abort)
+    map)
+  "Transient keymap for temporary LaTeX code edit buffers.")
+
+(defvar-local neo--latex-code-edit-source-buffer nil
+  "Source buffer for the current temporary LaTeX code edit buffer.")
+
+(defvar-local neo--latex-code-edit-start nil
+  "Start marker for the source range being edited.")
+
+(defvar-local neo--latex-code-edit-end nil
+  "End marker for the source range being edited.")
+
+(defvar-local neo--latex-code-edit-indent 0
+  "Shared indentation stripped from the current code edit buffer.")
+
+(defun neo--latex-code-edit-common-indent ()
+  "Return the least indentation among nonblank lines in the current buffer."
+  (save-excursion
+    (goto-char (point-min))
+    (let (min-indent)
+      (while (< (point) (point-max))
+        (unless (looking-at-p "[ \t]*$")
+          (setq min-indent
+                (if min-indent
+                    (min min-indent (current-indentation))
+                  (current-indentation))))
+        (forward-line 1))
+      (or min-indent 0))))
+
+(defun neo--latex-code-edit-shift-indentation (columns)
+  "Shift nonblank lines in the current buffer by COLUMNS."
+  (save-excursion
+    (goto-char (point-min))
+    (while (< (point) (point-max))
+      (unless (looking-at-p "[ \t]*$")
+        (let ((target-indent (max 0 (+ (current-indentation) columns))))
+          (back-to-indentation)
+          (delete-region (line-beginning-position) (point))
+          (indent-to target-indent)))
+      (forward-line 1))))
+
+(defun neo--latex-close-code-edit-buffer (message-text)
+  "Close the current temporary code edit buffer with MESSAGE-TEXT."
+  (set-buffer-modified-p nil)
+  (quit-window t)
+  (message "%s" message-text))
+
+(defun neo--latex-code-edit-abort ()
+  "Abort the current temporary code edit."
+  (interactive)
+  (neo--latex-close-code-edit-buffer "Aborted code block edit"))
+
+(defun neo--latex-code-edit-accept ()
+  "Write the current temporary code edit back to the source buffer."
+  (interactive)
+  (let ((source-buffer neo--latex-code-edit-source-buffer)
+        (start (and (markerp neo--latex-code-edit-start)
+                    (marker-position neo--latex-code-edit-start)))
+        (end (and (markerp neo--latex-code-edit-end)
+                  (marker-position neo--latex-code-edit-end)))
+        (indent neo--latex-code-edit-indent)
+        (edited-text (buffer-substring-no-properties (point-min) (point-max))))
+    (unless (buffer-live-p source-buffer)
+      (user-error "The original LaTeX buffer is no longer available"))
+    (unless (and start end)
+      (user-error "The original code range is no longer available"))
+    (when (> indent 0)
+      (setq edited-text
+            (with-temp-buffer
+              (insert edited-text)
+              (neo--latex-code-edit-shift-indentation indent)
+              (buffer-string))))
+    (with-current-buffer source-buffer
+      (save-excursion
+        (atomic-change-group
+          (delete-region start end)
+          (goto-char start)
+          (insert edited-text))))
+    (neo--latex-close-code-edit-buffer "Updated code block")))
+
+(defun neo--latex-open-code-edit-buffer (code-range)
+  "Open CODE-RANGE in a temporary `python-mode' buffer."
+  (let* ((source-buffer (current-buffer))
+         (start (car code-range))
+         (end (cdr code-range))
+         (buffer-name (format "*neo latex code: %s*" (buffer-name source-buffer)))
+         (source-text (buffer-substring-no-properties start end))
+         (start-marker (with-current-buffer source-buffer
+                         (copy-marker start)))
+         (end-marker (with-current-buffer source-buffer
+                       (copy-marker end)))
+         (buffer (generate-new-buffer buffer-name)))
+    (with-current-buffer buffer
+      (insert source-text)
+      (let ((common-indent (neo--latex-code-edit-common-indent)))
+        (when (> common-indent 0)
+          (neo--latex-code-edit-shift-indentation (- common-indent)))
+        (goto-char (point-min))
+        (python-mode)
+        (use-local-map
+         (make-composed-keymap neo--latex-code-edit-map (current-local-map)))
+        (setq-local header-line-format
+                    "C-c C-c accept edits   C-c C-k abort")
+        (setq-local neo--latex-code-edit-indent common-indent)
+        (setq-local neo--latex-code-edit-source-buffer source-buffer)
+        (setq-local neo--latex-code-edit-start start-marker)
+        (setq-local neo--latex-code-edit-end end-marker)))
+    (pop-to-buffer buffer)
+    (message "Edit code block, then press C-c C-c to accept or C-c C-k to abort.")))
+
+(defun neo/edit-code-block (code-range)
+  "Edit CODE-RANGE from the current starlark environment."
+  (interactive (list (neo/starlark--bounds-auctex-precise)))
+  (neo--latex-open-code-edit-buffer code-range))
 
 ;;; Note, no (provide 'neo-latex) here, extensions are loaded not required.
