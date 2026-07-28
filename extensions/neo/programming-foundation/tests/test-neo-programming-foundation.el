9b7565c2198393bdf1661c166e2570c3c1f220a6
Maurizio Vitale <mrz.vtl+github@gmail.com>
2026-07-28 13:22:09 -0400

    fix(emacs): preserve diagnostic tooltip popup
    Keep the existing tooltip presenter active while Eglot configures its separate hover surface, and cover both global and buffer-local routing.
    
    Beads: omega-11sv.87
    GitOrigin-RevId: 0af4fb5241a753b5fc93f2af142a3670b1e13d22

---
+++ extensions/neo/programming-foundation/tests/test-neo-programming-foundation.el
@@ -0,0 +1,50 @@
+;;; test-neo-programming-foundation.el --- Tests for programming-foundation -*- lexical-binding: t; -*-
+
+(require 'buttercup)
+(require 'cl-lib)
+(require 'eglot)
+(require 'tooltip)
+
+(defmacro neo/use-package (_name &rest _arguments)
+  "Ignore package declarations while loading the extension under test."
+  nil)
+
+(provide 'neo-programming-foundation-treesit)
+(provide 'neo-eglot-info)
+
+(defvar neo/framework-bootstrapped-p nil)
+(defvar neo/after-framework-bootstrap-hook nil)
+
+(defconst neo--programming-foundation-test-directory
+  (file-name-directory (or load-file-name buffer-file-name))
+  "Directory containing `test-neo-programming-foundation.el'.")
+
+(load-file
+ (expand-file-name "../neo-programming-foundation.el"
+                   neo--programming-foundation-test-directory))
+
+(describe "neo-programming-foundation"
+  (describe "Eglot hover presentation"
+    (it "preserves popup tooltip routing when Eglot starts"
+      (with-temp-buffer
+        (let ((show-help-function #'tooltip-show-help)
+              (eglot--managed-mode t))
+          (cl-letf (((symbol-function 'neo--eglot-ensure-hover-timer)
+                     #'ignore))
+            (neo--eglot-configure-eldoc))
+          (expect show-help-function :to-be #'tooltip-show-help)
+          (expect (local-variable-p 'show-help-function) :to-be nil))))
+
+    (it "preserves a buffer-local tooltip presenter across Eglot state changes"
+      (with-temp-buffer
+        (setq-local show-help-function #'tooltip-show-help)
+        (let ((eglot--managed-mode t))
+          (cl-letf (((symbol-function 'neo--eglot-ensure-hover-timer)
+                     #'ignore))
+            (neo--eglot-configure-eldoc)))
+        (let ((eglot--managed-mode nil))
+          (neo--eglot-configure-eldoc))
+        (expect show-help-function :to-be #'tooltip-show-help)
+        (expect (local-variable-p 'show-help-function) :to-be t)))))
+
+;;; test-neo-programming-foundation.el ends here
